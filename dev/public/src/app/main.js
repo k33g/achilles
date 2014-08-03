4908a54fa716a983e2bd849db950b9469de46e07
k33g <ph.charriere@gmail.com>
2014-08-03 08:44:14 +0200

    Router: read hash when start listening

--- dev/public/src/app/main.js
+++ dev/public/src/app/main.js
@@ -1,8 +1,36 @@
 import Router from '../Router';
+import Human from './models/Human';
+import Humans from './models/Humans';
+
+var router = new Router();
+
+router.add("/humans/{v}", (args)=>{
+  console.log("one human", args);
+  var human = new Human();
+  human.fetch(args[0]).then(() => { console.log(human); });
+
+})
+
+router.add("/humans", (args)=>{
+  console.log("all humans", args);
+  var humans = new Humans();
+  humans.fetch().then(() => { console.log(humans.models); });
+})
+
+/*<a href="#/">router.match("/");</a><br>*/
+
+router.add("/", (args)=>{
+  console.log("===HOME===")
+})
 
 
+/*<a href="#">router.match("");</a><br>*/
+router.add("", (args, hash)=>{ //all
+  console.log("###===SomeThingElse===###", args, hash)
+})
 
 
+router.listen()
 
 
 
